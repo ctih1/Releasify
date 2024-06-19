48071b8474712259910c8c553440babdc4b51108
ctih <nevalaonni@proton.me>
2024-06-19 20:54:48 +0300

    added logging

--- src/compiler.py
+++ src/compiler.py
@@ -1,12 +1,10 @@
 import os
-
+from tqdm import tqdm
 def compile(commands:list)->list:
     files:list=[]
-    for step in commands:
-        print(step)
+    for step in tqdm(commands):
+        tqdm.write(f"Running command for file {step['name']}")
         os.system(step["command"])
         files.append({"file":step["file"],"name":step["name"]})
     return files
     
-
-        
--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ import dotenv
 import requests
 import json
 import os
+from tqdm import tqdm
 from configuration import Configurator as conf
 import compiler
 
@@ -13,36 +14,37 @@ app = Flask(__name__)
 
 @app.route("/webhook",methods=["POST"])
 def webhook():
-	with open("response.json","w") as f: json.dump(request.json,f)
-	if(request.json.get("action")=="published"): # if the release is published, not deleted or mdified
-		print("Correct action")
-		id:int = int(request.json.get("release",{}).get("id",-1))
-		repo_username:str = str(request.json.get("repository",{}).get("full_name",""))
-		if(id==-1): return
-		repo_name = request.json.get("repository",{}).get("name")
-		print(repo_name)
-		conf_data = conf.load(repo_name)
-		print(conf_data)
-		files = compiler.compile(
-			commands=conf_data.get("steps"),
-		)
-		print(f"files: {files}")
-		for file in files:
-			print(file)
-			with open(file["file"],"rb") as f:
-				filedata=f.read()
-
-			response=requests.post(
-				f"https://uploads.github.com/repos/{repo_username}/releases/{str(id)}/assets?name={file['name']}",
-				data=filedata,
-				headers={
-					"User-Agent":"request",
-					"Accept":"application/vnd.github+json",
-					"Authorization": "Bearer "+gh_token,
-					"X-GitHub-Api-Version":"2022-11-28",
-					"Content-Type":"application/octet-stream"
-				})
-			print(f"recieved: {response.json()}")
-	print("Recieved webhook")
-	return "OK",200
+    tqdm.write("Webhook recieved!")
+    with open("response.json","w") as f: json.dump(request.json,f)
+    if(request.json.get("action")=="published"): # if the release is published, not deleted or mdified
+        id:int = int(request.json.get("release",{}).get("id",-1))
+        repo_username:str = str(request.json.get("repository",{}).get("full_name",""))
+        if(id==-1): 
+            tqdm.write("Release ID is not valid, aborting")
+            return
+        repo_name = request.json.get("repository",{}).get("name")
+        conf_data = conf.load(repo_name)
+        files = compiler.compile(
+            commands=conf_data.get("steps"),
+        )
+        for file in files:
+            tqdm.write(f"Loading {file['file']}...")
+            with open(file["file"],"rb") as f:
+                filedata=f.read()
+            tqdm.write(f"Uploading file {file['name']} to GitHub")
+            response=requests.post(
+                f"https://uploads.github.com/repos/{repo_username}/releases/{str(id)}/assets?name={file['name']}",
+                data=filedata,
+                headers={
+                    "User-Agent":"request",
+                    "Accept":"application/vnd.github+json",
+                    "Authorization": "Bearer "+gh_token,
+                    "X-GitHub-Api-Version":"2022-11-28",
+                    "Content-Type":"application/octet-stream"
+                })
+            if(response.status_code!=200):
+                tqdm.write(f"Upload failed: {response.json()}")
+            else:
+                tqdm.write(f"Succesfully uploaded {file['name']}")
+    return "OK",200
 app.run()
